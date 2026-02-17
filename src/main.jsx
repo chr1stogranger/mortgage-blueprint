5d403c2d2c0942782cd152d107932830a6971b67
Christo <chr1stogranger@gmail.com>
2026-02-16 22:07:51 -0800

    wire FRED API key from env to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+window.__FRED_API_KEY__ = import.meta.env.VITE_FRED_API_KEY || "";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
